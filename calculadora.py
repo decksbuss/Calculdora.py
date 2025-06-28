6d78141b7027447f2580a831473dc26a793b2fd8
DecksBuss <derickbuss@gmail.com>
2025-06-28 07:41:58 -0300

    Add files via upload

---
+++ calculadora.py
@@ -0,0 +1,64 @@
+import tkinter
+from tkinter import *
+from tkinter import ttk
+
+
+#cores
+cor1= "#010c14"
+cor2= '#ffffff'
+cor3= '#0914e0'
+cor4= '#676b85'
+cor5= '#c2410a'
+
+
+janela= Tk()
+janela.title('Calculadora')
+janela.geometry('270x400')
+janela.config(bg=cor1)
+
+frame_tela= Frame(janela, width=270, height=70, bg=cor3 )
+frame_tela.grid(row=0, column=0)
+
+frame_corpo= Frame(janela, width=270, height=330 )
+frame_corpo.grid(row=1, column=0)
+#criando botões
+b1= Button(frame_corpo, text='C', width=14, height=3, bg=cor4, font=('Ivy 13 bold'), relief=RAISED)
+b1.place(x=0, y=0)
+b2= Button(frame_corpo, text='%', width=7, height=3, bg=cor4, font=('Ivy 13 bold'), relief=RAISED)
+b2.place(x=130, y=0)
+b3= Button(frame_corpo, text='/', width=7, height=3, bg=cor5, fg=cor2, font=('Ivy 13 bold'), relief=RAISED)
+b3.place(x=195, y=0)
+b4= Button(frame_corpo, text='7', width=7, height=3, bg=cor4, font=('Ivy 13 bold'), relief=RAISED)
+b4.place(x=0, y=66)
+b5= Button(frame_corpo, text='8', width=7, height=3, bg=cor4, font=('Ivy 13 bold'), relief=RAISED)
+b5.place(x=65, y=66)
+b6= Button(frame_corpo, text='9', width=7, height=3, bg=cor4, font=('Ivy 13 bold'), relief=RAISED)
+b6.place(x=130, y=66)
+b7= Button(frame_corpo, text='*', width=7, height=3, bg=cor5, fg=cor2, font=('Ivy 13 bold'), relief=RAISED)
+b7.place(x=195, y=66)
+b8= Button(frame_corpo, text='4', width=7, height=3, bg=cor4, font=('Ivy 13 bold'), relief=RAISED)
+b8.place(x=0, y=132)
+b9= Button(frame_corpo, text='5', width=7, height=3, bg=cor4, font=('Ivy 13 bold'), relief=RAISED)
+b9.place(x=65, y=132)
+b10= Button(frame_corpo, text='6', width=7, height=3, bg=cor4, font=('Ivy 13 bold'), relief=RAISED)
+b10.place(x=130, y=132)
+b11= Button(frame_corpo, text='-', width=7, height=3, bg=cor5, fg=cor2, font=('Ivy 13 bold'), relief=RAISED)
+b11.place(x=195, y=132)
+b12= Button(frame_corpo, text='1', width=7, height=3, bg=cor4, font=('Ivy 13 bold'), relief=RAISED)
+b12.place(x=0, y=198)
+b13= Button(frame_corpo, text='2', width=7, height=3, bg=cor4, font=('Ivy 13 bold'), relief=RAISED)
+b13.place(x=65, y=198)
+b14= Button(frame_corpo, text='3', width=7, height=3, bg=cor4, font=('Ivy 13 bold'), relief=RAISED)
+b14.place(x=130, y=198)
+b15= Button(frame_corpo, text='+', width=7, height=3, bg=cor5, fg=cor2, font=('Ivy 13 bold'), relief=RAISED)
+b15.place(x=195, y=198)
+b16= Button(frame_corpo, text='0', width=14, height=3, bg=cor4, font=('Ivy 13 bold'), relief=RAISED)
+b16.place(x=0, y=264)
+b17= Button(frame_corpo, text='.', width=7, height=3, bg=cor4, font=('Ivy 13 bold'), relief=RAISED)
+b17.place(x=130, y=264)
+b18= Button(frame_corpo, text='=', width=7, height=3, bg=cor5, fg=cor2, font=('Ivy 13 bold'), relief=RAISED)
+b18.place(x=195, y=264)
+
+janela.mainloop()
+
+
